813ec6a4c7a12d9c4c6d15de8790ef01dfd2ad30
Andrew Osheroff <andrewosh@gmail.com>
2025-08-04 12:12:15 +0200

    Add raw rocksdb benchmarking too

--- generate.mjs
+++ generate.mjs
@@ -1,11 +1,15 @@
 import HyperDB from 'hyperdb'
+import RocksDB from 'rocksdb-native'
 import spec from './spec/hyperdb/index.js'
 
 const BATCH_SIZE = 10000
 
-await generateDatabase('./dbs/1e6', 1e6)
-await generateDatabase('./dbs/1e5', 1e5)
-await generateDatabase('./dbs/1e4', 1e4)
+// await generateDatabase('./dbs/1e6', 1e6)
+// await generateDatabase('./dbs/1e5', 1e5)
+// await generateDatabase('./dbs/1e4', 1e4)
+await generateRawDatabase('./dbs/raw-1e6', 1e6)
+await generateRawDatabase('./dbs/raw-1e5', 1e5)
+await generateRawDatabase('./dbs/raw-1e4', 1e4)
 
 async function generateDatabase (dir, size) {
   const db = HyperDB.rocks(dir, spec)
@@ -26,5 +30,24 @@ async function generateDatabase (dir, size) {
   }
 
   await tx.flush()
+  await db.close()
+}
+
+async function generateRawDatabase (dir, size) {
+  const db = new RocksDB(dir)
+
+  let tx = db.write()
+  for (let i = 0; i < size; i++) {
+    tx.tryPut('' + i, Buffer.alloc(1024).fill(Math.floor(Math.random() * 256)))
+    if ((i % BATCH_SIZE) === 0) {
+      console.log('inserted:', i)
+      await tx.flush()
+      await tx.destroy()
+      tx = db.write()
+    }
+  }
+
+  await tx.flush()
+  await tx.destroy()
   await db.close()
 }
--- index.mjs
+++ index.mjs
@@ -1,11 +1,15 @@
 import HyperDB from 'hyperdb'
+import RocksDB from 'rocksdb-native'
 import spec from './spec/hyperdb/index.js'
 
 const DURATION = 10 * 1000 // 10s
 
-await bench('./dbs/1e6', 1e6)
-await bench('./dbs/1e5', 1e5)
-await bench('./dbs/1e4', 1e4)
+// await bench('./dbs/1e6', 1e6)
+// await bench('./dbs/1e5', 1e5)
+// await bench('./dbs/1e4', 1e4)
+await benchRaw('./dbs/raw-1e6', 1e6)
+await benchRaw('./dbs/raw-1e5', 1e5)
+await benchRaw('./dbs/raw-1e4', 1e4)
 
 async function bench (dir, count) {
   const db = HyperDB.rocks(dir, spec)
@@ -15,6 +19,24 @@ async function bench (dir, count) {
   while (true) {
     const key = Math.floor(Math.random() * count)
     const res = await db.get('@x/b', { b: key })
+    if (!res) throw new Error(`Key is not there but should be there: ${key}`)
+    i++
+    duration = Date.now() - start
+    if (duration >= DURATION) break
+  }
+  const rate = (i / duration) * 1000
+  console.log(`(${dir}) Read ${i} records in ${duration / 1000}s (${rate} records/s)`)
+  await db.close()
+}
+
+async function benchRaw (dir, count) {
+  const db = new RocksDB(dir)
+  const start = Date.now()
+  let duration = 0
+  let i = 0
+  while (true) {
+    const key = '' + Math.floor(Math.random() * count)
+    const res = await db.get(key)
     if (!res) throw new Error(`Key is not there but should be there: ${key}`)
     i++
     duration = Date.now() - start
